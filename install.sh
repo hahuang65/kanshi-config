b5884795abac810377e19913fa216642311e0693
Howard Huang <hao@hwrd.me>
2023-09-09 12:00:23 -0700

    Use kanshi as a systemd unit

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 if [ $(uname) = "Linux" ]; then
   mkdir -p "${HOME}/.config/kanshi"
   ln -sf "${PWD}/config" "${HOME}/.config/kanshi/"
+
+  mkdir -p "${HOME}/.config/systemd/user/"
+  ln -sf "${PWD}/kanshi.service" "${HOME}/.config/systemd/user/kanshi.service"
 fi
